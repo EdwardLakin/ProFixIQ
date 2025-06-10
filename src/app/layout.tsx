e450b85401c5b20c00f9aa1b21ed6909049e8f11
EdwardLakin <edwardlakin35@gmail.com>
2025-06-10 02:16:00 +0000

    Moved lib to src/lib, updated .gitignore

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-background text-foreground">
+        {children}
+      </body>
+    </html>
+  );
+}
