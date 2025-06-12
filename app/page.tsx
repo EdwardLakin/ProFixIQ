b4335ffb2ce8eba85cb2352ae9b104c638f3f691
EdwardLakin <edwardlakin35@gmail.com>
2025-06-12 19:05:44 +0000

    Add ProFixIQ landing page route at /app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+import ProFixIQLanding from '../components/ProFixIQLanding';
+
+export default function Page() {
+  return (
+    <div className="p-4">
+      <ProFixIQLanding />
+    </div>
+  );
+}
