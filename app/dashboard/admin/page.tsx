a51596531f3863fd57621317f6f50360dac0c7a8
EdwardLakin <edwardlakin35@gmail.com>
2025-09-08 16:16:17 +0000

    removed role app pages

--- app/dashboard/admin/page.tsx
+++
@@ -1,4 +0,0 @@
-"use client";
-import React from "react";
-import AdminDashboardClient from "@/features/dashboard/app/dashboard/admin/AdminDashboardClient";
-export default function Page(){ return <AdminDashboardClient/>; }
