d9497be9a86b5ad43ac823399939049710901854
EdwardLakin <edwardlakin35@gmail.com>
2025-09-20 02:10:30 +0000

    chore(work-orders): remove obsolete view/[id] page after merge into [id]/page

--- app/work-orders/view/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Page({ params }: { params: { id: string } }) {
-  // Send legacy route to the consolidated detail page in "tech" mode by default.
-  redirect();
-}
