7ce11fb189dd493d17e26fac2424a52fbaa87d7c
EdwardLakin <edwardlakin35@gmail.com>
2025-09-29 15:28:03 +0000

    removed unused import from Task Provider

--- features/launcher/tasks/TasksProvider.tsx
+++ features/launcher/tasks/TasksProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createContext, useCallback, useContext, useEffect, useMemo, useRef, useState } from "react";
+import { createContext, useCallback, useContext, useEffect, useMemo, useState } from "react";
 
 export type Task = {
   route: string;
