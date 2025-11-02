24e9261bdbc2201c8678bce0bb5e64cc2e7d1f97
EdwardLakin <edwardlakin35@gmail.com>
2025-11-02 03:07:11 +0000

    added wrapper fir generic inspection screen

---
+++ app/inspections/genericInspection/page.tsx
@@ -0,0 +1,4 @@
+// Mounts GenericInspectionScreen at /inspections/generic
+export { default } from "@/features/inspections/screens/GenericInspectionScreen";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
