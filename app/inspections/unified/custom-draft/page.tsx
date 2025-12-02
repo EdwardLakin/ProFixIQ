d04d25c75e2e63dd36b484ed860f463b9edc592b
edward lakin <edwardlakin35@gmail.com>
2025-12-02 22:41:17 +0000

    removed all unified inspection system refernces and files.

--- app/inspections/unified/custom-draft/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import UnifiedCustomDraftPage from "@/features/inspections/unified/custom-draft/page";
-
-export default function UnifiedCustomDraftRoute() {
-  return <UnifiedCustomDraftPage />;
-}
