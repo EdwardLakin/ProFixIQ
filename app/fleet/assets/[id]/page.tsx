3e9d6a6de5382cad80be52c5913d1725d34ce5b2
Edward Lakin <edwardlakin35@gmail.com>
2026-01-01 02:27:04 +0000

    updated assets id page

--- app/fleet/assets/[id]/page.tsx
+++ app/fleet/assets/[id]/page.tsx
@@ -5,19 +5,6 @@ export const revalidate = 0;
 
 import AssetDetailScreen from "@/features/fleet/components/AssetDetailScreen";
 
-type AssetDetailPageProps = {
-  params: {
-    id: string;
-  };
-};
-
-export default function Page({ params }: AssetDetailPageProps) {
-  const unitId = params.id;
-
-  // If somehow there's no id, render nothing (or you could throw notFound()).
-  if (!unitId) {
-    return null;
-  }
-
-  return <AssetDetailScreen unitId={unitId} />;
+export default function Page({ params }: any) {
+  return <AssetDetailScreen unitId={params?.id} />;
 }
