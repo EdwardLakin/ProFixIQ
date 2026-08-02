0b9d66fc8f364fd1fa84552e69ac0bb802baae6f
EdwardLakin <edwardlakin35@gmail.com>
2026-08-02 09:31:29 -0600

    Surface operational timeline on work orders

--- app/work-orders/[id]/page.tsx
+++ app/work-orders/[id]/page.tsx
@@ -1,8 +1,15 @@
 // app/work-orders/[id]/page.tsx
+import WorkOrderOperationalTimelineDock from "@/features/operations/components/WorkOrderOperationalTimelineDock";
 import WorkOrderIdClient from "./Client";
+
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 export default function Page() {
-  return <WorkOrderIdClient />;
+  return (
+    <>
+      <WorkOrderIdClient />
+      <WorkOrderOperationalTimelineDock />
+    </>
+  );
 }
