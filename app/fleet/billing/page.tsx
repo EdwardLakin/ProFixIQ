165ef4312f4109cc4246cf3a2c4022d17aef7228
EdwardLakin <edwardlakin35@gmail.com>
2026-08-02 18:05:50 -0600

    Add staff fleet billing page

---
+++ app/fleet/billing/page.tsx
@@ -0,0 +1,5 @@
+import FleetBillingWorkspace from "@/features/fleet/components/FleetBillingWorkspace";
+
+export default function FleetBillingPage() {
+  return <FleetBillingWorkspace routePrefix="/fleet" />;
+}
