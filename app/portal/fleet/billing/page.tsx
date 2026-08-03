c911920779586394797eba6f0936615de7385fb1
EdwardLakin <edwardlakin35@gmail.com>
2026-08-02 18:05:57 -0600

    Add portal fleet billing page

---
+++ app/portal/fleet/billing/page.tsx
@@ -0,0 +1,5 @@
+import FleetBillingWorkspace from "@/features/fleet/components/FleetBillingWorkspace";
+
+export default function PortalFleetBillingPage() {
+  return <FleetBillingWorkspace routePrefix="/portal/fleet" />;
+}
