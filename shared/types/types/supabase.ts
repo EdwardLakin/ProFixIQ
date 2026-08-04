b307dd1d59c7903be73cdde8ac0db72dff9dd886
EdwardLakin <edwardlakin35@gmail.com>
2026-08-04 00:06:57 -0600

    chore(types): add PO receipt operation id

--- shared/types/types/supabase.ts
+++ shared/types/types/supabase.ts
@@ -15706,6 +15706,7 @@ export type Database = {
       receive_po_part_and_allocate: {
         Args: {
           p_location_id: string
+          p_operation_id?: string
           p_part_id: string
           p_po_id: string
           p_qty: number
