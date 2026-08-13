897d2ac96d58a50f2d1be8235d2be9c5274a23c6
EdwardLakin <edwardlakin35@gmail.com>
2026-08-13 15:57:32 -0600

    export technician CoPilot repair session foundation

---
+++ features/copilot/technician/index.ts
@@ -0,0 +1,24 @@
+export {
+  applyRepairEvent,
+  createEmptyRepairContext,
+  reduceRepairEvents,
+} from "./session/reduceRepairContext";
+
+export {
+  REPAIR_EVENT_SOURCES,
+  REPAIR_EVENT_TYPES,
+  REPAIR_SESSION_MODES,
+  REPAIR_SESSION_STATUSES,
+  type KnownRepairEventType,
+  type RepairComponentMemory,
+  type RepairContextState,
+  type RepairDtc,
+  type RepairEventSource,
+  type RepairEventType,
+  type RepairEvidence,
+  type RepairMeasurement,
+  type RepairObservation,
+  type RepairSessionEvent,
+  type RepairSessionMode,
+  type RepairSessionStatus,
+} from "./session/types";
