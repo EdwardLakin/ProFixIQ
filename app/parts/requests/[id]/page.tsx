fbf031a0c50202773aa9dcc2dbe004abe61f0c0f
EdwardLakin <edwardlakin35@gmail.com>
2026-09-18 07:53:03 -0600

    Use compact header in Parts Workspace center pane

--- app/parts/requests/[id]/page.tsx
+++ app/parts/requests/[id]/page.tsx
@@ -2455,6 +2455,7 @@ export default function PartsRequestsForWorkOrderPage(): JSX.Element {
                     <div key={r.req.id} className={glassCard}>
                       <PartsRequestWorkbench
                         model={model}
+                        compactHeader
                         onAddItem={async () => {
                           await addRow(r.req.id);
                         }}
